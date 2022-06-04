52721bbd35b3b9c88ccc0f944f9f2be5feb6379f
yunqi <yunqi.mail@qq.com>
2022-06-04 14:14:44 +0800

    mit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -139,7 +139,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2022, // 博客创建年份
       copyrightInfo:
-        'yunqi | <a href="https://github.com/naturalCloud/naturalCloud-naturalCloud.github.io/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+        'yunqi | <a href="https://github.com/naturalCloud/naturalCloud.github.io/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
